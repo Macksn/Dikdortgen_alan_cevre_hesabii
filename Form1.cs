837f30ae5133b63f9e6d0d0cdf2e216547e3acb8
Macksn <itsmacksn@yahoo.com>
2024-03-24 22:35:16 +0300

    Dikdortgen_alan_cevre_hesabi
    Dikdörtgenin alan ve çevre hesabı

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dikdörtgenin_alanı_ve_çevre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int uzunkenar, kısakenar, alan;
+            uzunkenar = Convert.ToInt16(textBox1.Text);
+            kısakenar = Convert.ToInt16(textBox2.Text);
+            alan = uzunkenar * kısakenar;
+            label3.Text = alan.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int uzunkenar, kısakenar, çevre;
+            uzunkenar = Convert.ToInt16(textBox1.Text);
+            kısakenar = Convert.ToInt16(textBox2.Text);
+            çevre =  (uzunkenar + kısakenar)  * 2;
+            label3.Text = çevre.ToString();
+
+                
+        }
+    }
+}
